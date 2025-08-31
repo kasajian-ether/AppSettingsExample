1be1fe8d24c127302b2f1bbf3d92666d7af6eda9
dev <dev@dev.com>
2025-08-30 20:57:10 -0700

    try

--- AppSettingsExample/Program.cs
+++ AppSettingsExample/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace AppSettingsExample
+﻿using Microsoft.Extensions.Configuration;
+
+namespace AppSettingsExample
 {
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var getcur = Directory.GetCurrentDirectory();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+
+            // Access configuration values
+            string appName = configuration["ApplicationName"];
+            string defaultLogLevel = configuration["Logging:LogLevel:Default"];
+
+            Console.WriteLine($"Application Name: {appName}");
+            Console.WriteLine($"Default Log Level: {defaultLogLevel}");
+
+            // Read into an object
+            ApiTester settings = new();
+            configuration.GetSection(nameof(ApiTester)).Bind(settings);
+
         }
+    }
+
+    public class ApiTester
+    {
+        public string[] SupportedMethods { get; set; }
+        public int MaxRetries { get; set; }
+        public string BaseUrl { get; set; }
     }
 }
